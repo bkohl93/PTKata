eea6bb11a61cec01543a4fff1770b82ae6838474
Ben Kohl <bkohl93@gmail.com>
2016-01-21 16:34:34 -0500

    added negative one test case to arabicToRoman.

--- roman.cpp
+++ roman.cpp
@@ -3,7 +3,7 @@
 // takes an arabic integer and returns its roman numeral representation
 std::string arabicToRoman(int n)
 {
-  return "No roman numeral representation of zero exists.";
+  return "No roman numeral representation of integers less than one exist.";
 }
 
 //takes a string of roman numerals and returns its arabic integer representation
@@ -14,7 +14,9 @@ int romanToArabic(std::string s)
 
 int main()
 {
-  (arabicToRoman(0) == "No roman numeral representation of zero exists.") ? std::cout << "passed" : std::cout << "failed";
+  (arabicToRoman(0) == "No roman numeral representation of integers less than one exist." &&
+  arabicToRoman(-1) == "No roman numeral representation of integers less than one exist."
+  ) ? std::cout << "passed" : std::cout << "failed";
 
 
   std::cout << std::endl;

f25766a273b4ba4f1f6addd2d20af82d28d268ef
Ben Kohl <bkohl93@gmail.com>
2016-01-21 16:21:44 -0500

    added zero test case to arabicToRoman

--- roman.cpp
+++ roman.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
+// takes an arabic integer and returns its roman numeral representation
 std::string arabicToRoman(int n)
 {
-  return "";
+  return "No roman numeral representation of zero exists.";
 }
 
+//takes a string of roman numerals and returns its arabic integer representation
 int romanToArabic(std::string s)
 {
   return 0;
@@ -12,5 +14,9 @@ int romanToArabic(std::string s)
 
 int main()
 {
+  (arabicToRoman(0) == "No roman numeral representation of zero exists.") ? std::cout << "passed" : std::cout << "failed";
+
+
+  std::cout << std::endl;
   return 0;
 }
